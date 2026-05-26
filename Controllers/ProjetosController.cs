7faa11418ac7a9f66768a31194f902c2bd247c8f
Elano Santos <elanosantos.santos098@gmail.com>
2026-05-26 18:01:20 -0300

    atividade senai, parte 2

--- Controllers/ProjetosController.cs
+++ Controllers/ProjetosController.cs
@@ -1,6 +1,5 @@
 using Exo.WebApi.Models;
 using Exo.WebApi.Repositories;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 
@@ -8,14 +7,11 @@ namespace Exo.WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-
     public class ProjetosController : ControllerBase
     {
-        private readonly ProjetoRepository
-        _projetoRepository;
+        private readonly ProjetoRepository _projetoRepository;
 
-        public ProjetosController(ProjetoRepository
-        projetoRepository)
+        public ProjetosController(ProjetoRepository projetoRepository)
         {
             _projetoRepository = projetoRepository;
         }
@@ -24,6 +20,47 @@ namespace Exo.WebApi.Controllers
         public IActionResult Listar()
         {
             return Ok(_projetoRepository.Listar());
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Projeto projeto)
+        {
+            _projetoRepository.Cadastrar(projeto);
+            return StatusCode(201);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            Projeto projeto = _projetoRepository.BuscarporId(id);
+
+            if (projeto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(projeto);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Projeto projeto)
+        {
+            _projetoRepository.Atualizar(id, projeto);
+            return StatusCode(204);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _projetoRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
         }
     }
 }
